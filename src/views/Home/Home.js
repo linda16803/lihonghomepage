c50b13a1c2014330f420df5576cb70acfa788d88
Huang,Lihong (lh9327) <lh9327@us.att.com>
2022-01-04 19:16:38 -0500

    [GOODLUCK] add new update code personal respo.

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -12,7 +12,7 @@ import LocalOffer from "@material-ui/icons/LocalOffer";
 import Update from "@material-ui/icons/Update";
 
 import Accessibility from "@material-ui/icons/Accessibility";
-import { urlString } from "variables/apiWrapper";
+import { urlString } from "variables/apiWrapper";./
 
 // core components
 import GridItem from "components/Grid/GridItem.js";
@@ -24,43 +24,44 @@ import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 
 import CardFooter from "components/Card/CardFooter.js";
-import Image from '../../assets/img/homepage1.png';
-import Paper from '@material-ui/core/Paper';
-
-
+import Image from "../../assets/img/homepage1.png";
+import Paper from "@material-ui/core/Paper";
 
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
 const useStyles = makeStyles(styles);
 const backgroundstyles = {
   paperContainer: {
-      backgroundImage: `url(${Image})`,
-      height: '100vh'
-  }
+    backgroundImage: `url(${Image})`,
+    height: "100vh",
+  },
 };
 
 export default function Home() {
   const classes = useStyles();
   return (
-   
-    
-      <Paper style={backgroundstyles.paperContainer}>
-        <CardBody>
-        <div >
-          <h1 className={classes.cardTitleWhite}><b>Hello World</b> </h1>
+    <Paper style={backgroundstyles.paperContainer}>
+      <CardBody>
+        <div>
+          <h1 className={classes.cardTitleWhite}>
+            <b>Hello World</b>{" "}
+          </h1>
         </div>
         <div>
-        
-          <h3 className={classes.cardTitleWhite}> This is Lihong's Small World</h3>
+          <h3 className={classes.cardTitleWhite}>
+            {" "}
+            This is Lihong's Small World
+          </h3>
         </div>
         <div>
-        
-          <h4 className={classes.cardTitleWhite}> Software developer as my career</h4> 
-          <h4 className={classes.cardTitleWhite}>Traveling as my enjoy  </h4>
-          <h4 className={classes.cardTitleWhite}>Singing as my hobby!  </h4>
+          <h4 className={classes.cardTitleWhite}>
+            {" "}
+            Software developer as my career
+          </h4>
+          <h4 className={classes.cardTitleWhite}>Traveling as my enjoy </h4>
+          <h4 className={classes.cardTitleWhite}>Singing as my hobby! </h4>
         </div>
-        </CardBody>
-      </Paper>
-   
+      </CardBody>
+    </Paper>
   );
 }
